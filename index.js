aee9d1a9b2d77a51c33ff270330098b0f6c8d8b4
longthai <thaitam.long94@gmail.com>
2024-12-12 04:07:29 +0200

    Adding spin animation and disable other button when spinning

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 // Quay
-const spin = async () => {
-    const numOfItem = 3;
-    const itemImages = document.getElementById('item-img');
-    const images = [];
-    const values = [];
+let intervalId;
+const resetBtn = document.getElementById('btn-reset');
+const spinBtn = document.getElementById('btn-spin');
 
-    for (let i = 0; i < numOfItem; i++) {
-        const value = Math.floor(Math.random()*6) + 1;
-        values.push(value);
-        images.push(`<img src="./images/${value}.png" class="img-count">`)
+const spin = () => {
+    let spinRes = [];
+    document.getElementById('message').innerHTML = "";
+    resetBtn.disabled = true;
+    spinBtn.disabled = true;
+
+    clearInterval(intervalId);
+
+    if (betRes.length < 3) {
+        document.getElementById('message').innerHTML = "<h2>Vui lòng đặt đủ ô trước khi quay</h2>"
+        spinBtn.disabled = false;
+    } else {
+        intervalId = setInterval( () => {
+            const numOfItem = 3;
+            const itemImages = document.getElementById('item-img');
+            const images = [];
+            const values = [];
+
+            for (let i = 0; i < numOfItem; i++) {
+                const value = Math.floor(Math.random()*3) + 1;
+                values.push(value);
+                images.push(`<img src="./images/${value}.png" class="img-count">`)
+            }
+            spinRes = values;
+            itemImages.innerHTML=images.join('')
+        }, 100);
+
+        setTimeout(() => {
+            console.log(`Kết quả quay: ${spinRes}`);
+            console.log(`Dự đoán của bạn: ${betRes}`);
+
+            clearInterval(intervalId);
+            compareResult(spinRes, betRes);
+            resetBtn.disabled = false;
+            spinBtn.disabled = false;
+        }, 100);
     }
-    itemImages.innerHTML=images.join('')
-
-    let values1 = values.slice().sort();
-    let betResult1 = betResult.slice().sort();
-    
-    console.log(`Kết quả quay: ${values}`);
-    console.log(`Dự đoán của bạn: ${betResult}`);
-
-    compareResult(values1, betResult1);
 }
 
 //Đếm 
 const btns = [...document.querySelectorAll('button.btn-click-count')],
 btnsClicksCountArr = [btns.length];
-let betResult = [];
+let betRes = [];
 
  for (const [i, el] of btns.entries()) {
      btnsClicksCountArr[i] = 0;
@@ -39,7 +60,7 @@ let betResult = [];
 
          if (btnsClicksCountArr[i] < 3 && sum < 3) {
              el.querySelector('div').textContent = ++btnsClicksCountArr[i]
-             betResult.push(betItem);
+             betRes.push(betItem);
          } else {
              el.querySelector('div').textContent = btnsClicksCountArr[i];
          }
@@ -48,7 +69,7 @@ let betResult = [];
 
 //  Reset cửa đặt
 const resetCount = () => {
-    betResult=[]
+    betRes=[]
     //window.location.reload();
     btns.forEach((el, i) => {
          btnsClicksCountArr[i] = 0;
@@ -61,15 +82,14 @@ const resetCount = () => {
 // So sánh cửa đặt và kết quả quay
 const compareResult = (a1, a2) => {
     let msg = "";
+    
+    let s1 = a1.slice().sort();
+    let s2 = a2.slice().sort();
 
-    if (a1.length !== a2.length) {
-        msg = "Vui lòng đặt đủ ô trước khi quay"
+    if (JSON.stringify(s1) == JSON.stringify(s2)) {
+        msg = 'Bạn đã thắng !!!'
     } else {
-        if (JSON.stringify(a1) == JSON.stringify(a2)) {
-            msg = 'Bạn đã thắng !!!'
-        } else {
-            msg = 'Chúc may mắn lần sau!'
-        }
+        msg = 'Chúc may mắn lần sau!'
     }
     
     console.log(msg)
