2eab8ca03439ee49a71289412036150069342154
longthai <thaitam.long94@gmail.com>
2024-12-12 04:38:14 +0200

    Adding code comment

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const spinBtn = document.getElementById('btn-spin');
 const spin = () => {
     let spinRes = [];
     document.getElementById('message').innerHTML = "";
+
+    //Disable 2 nút quay và reset khi đang quay
     resetBtn.disabled = true;
     spinBtn.disabled = true;
 
     clearInterval(intervalId);
-
+    //Kiểm tra xem người dùng đã đặt cửa chưa.
     if (betRes.length < 3) {
         document.getElementById('message').innerHTML = "<h2>Vui lòng đặt đủ ô trước khi quay</h2>"
         spinBtn.disabled = false;
@@ -21,8 +23,12 @@ const spin = () => {
             const images = [];
             const values = [];
 
+            // Tạo một vòng loop để chọn random item và item.value.
+            // display ảnh vào giao diện.
+            // value vào spinRes để so sánh với betRes.
+            // Hiệu ứng xoay tôm cua cá, mỗi nháy 0.1s.
             for (let i = 0; i < numOfItem; i++) {
-                const value = Math.floor(Math.random()*3) + 1;
+                const value = Math.floor(Math.random()*6) + 1;
                 values.push(value);
                 images.push(`<img src="./images/${value}.png" class="img-count">`)
             }
@@ -30,6 +36,7 @@ const spin = () => {
             itemImages.innerHTML=images.join('')
         }, 100);
 
+        //Hiện kết quả spin, tin nhắn, so sánh sau 5s 
         setTimeout(() => {
             console.log(`Kết quả quay: ${spinRes}`);
             console.log(`Dự đoán của bạn: ${betRes}`);
@@ -38,36 +45,43 @@ const spin = () => {
             compareResult(spinRes, betRes);
             resetBtn.disabled = false;
             spinBtn.disabled = false;
-        }, 100);
+        }, 5000);
     }
 }
 
-//Đếm 
+//Đếm
 const btns = [...document.querySelectorAll('button.btn-click-count')],
 btnsClicksCountArr = [btns.length];
 let betRes = [];
 
- for (const [i, el] of btns.entries()) {
-     btnsClicksCountArr[i] = 0;
-     el.querySelector('div').textContent = btnsClicksCountArr[i];
+//el = button hiện tại trong array, i là index trong array đó
+for (const [i, el] of btns.entries()) {
+    //reset bộ đếm ban đầu của tất cả các button về 0
+    btnsClicksCountArr[i] = 0;
+    el.querySelector('div').textContent = btnsClicksCountArr[i];
 
-     el.addEventListener('click', () => {
-         const betItem = Number(el.value);
-         sum = 0;
-         for (let j = 0; j < btnsClicksCountArr.length; j++) {
-             sum += btnsClicksCountArr[j];
-         }
+    //click event handler cho các button
+    el.addEventListener('click', () => {
+        const betItem = Number(el.value);
+        let sum = 0;
+        //sum là tổng số lần người chơi click vào button, dùng để kiểm tra điều kiện
+        for (let j = 0; j < btnsClicksCountArr.length; j++) {
+            sum += btnsClicksCountArr[j];
+        }
 
-         if (btnsClicksCountArr[i] < 3 && sum < 3) {
-             el.querySelector('div').textContent = ++btnsClicksCountArr[i]
-             betRes.push(betItem);
-         } else {
-             el.querySelector('div').textContent = btnsClicksCountArr[i];
-         }
-     });
- }
+        //kiểm tra điều kiện mỗi button chỉ được click tối đa 3 lần, và tổng số các button click tối đá là 3. 
+        //Thỏa mãn thì tính +1 lần click.
+        //Thêm cửa đặt của người chơi vào arr betRes với value từ button.btn-click-count
+        if (btnsClicksCountArr[i] < 3 && sum < 3) {
+            el.querySelector('div').textContent = ++btnsClicksCountArr[i]
+            betRes.push(betItem);
+        } else {
+            el.querySelector('div').textContent = btnsClicksCountArr[i];
+        }
+    });
+}
 
-//  Reset cửa đặt
+//  Reset cửa đặt, bộ đếm và tin nhắn về rỗng
 const resetCount = () => {
     betRes=[]
     //window.location.reload();
